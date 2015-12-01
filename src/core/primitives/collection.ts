5e61d8c3d0168e16bddbab62193fd81b0e6f8935
cuddlevictim one <cuddlevictim one>
2015-12-01 01:15:44 +0100

    minor progress

--- src/core/primitives/collection.ts
+++ src/core/primitives/collection.ts
@@ -30,3 +30,9 @@ export const defaults: Function = (...collections): Object => {
 export const forEachKey: Function = (collection: Object, assign:any): void => {
   Object.keys(collection).forEach( assign );
 }
+
+// @quick&dirty - get object property from string 
+// @example: var o = {a:{b:{c:'found me!'}}}; resolve(o, 'a.b.c') , resolve(o, 'a[b]["c"]'), etc... resolve(o, '[a][][][""b]c[]][]')
+export const resolve: Function = (obj, prop) => {
+	return prop.split(/\[|\]|\.|'|"/g).filter((v)=>{return v}).reduce((a,b)=>{ return a[b] },obj);
+}
--- src/core/primitives/object.ts
+++
@@ -1,5 +0,0 @@
-// @quick&dirty - get object property from string 
-// @example: var o = {a:{b:{c:'found me!'}}}; resolve(o, 'a.b.c') , resolve(o, 'a[b]["c"]'), etc... resolve(o, '[a][][][""b]c[]][]')
-export const resolve: Function = (obj, prop) => {
-	return prop.split(/\[|\]|\.|'|"/g).filter((v)=>{return v}).reduce((a,b)=>{ return a[b] },obj);
-}
