1f8abd7d42893986048b56f5efb9ad1d62b028b8
cuddlevictim one <dizzyone99@gmail.com>
2015-12-02 01:17:15 +0100

    deleted commented code

--- src/core/dom/manipulation.ts
+++ src/core/dom/manipulation.ts
@@ -2,27 +2,6 @@ import {interpolate} from "./../primitives/string";
 
 export const Δ = document.querySelectorAll.bind(document);
 
-// export const interpolateHTML: Function = (data: any, type?: string, element?: HTMLElement, delimiterRegEx?: RegExp): HTMLElement => {
-// 	let interpolateType = `[data-interpolate${(!type) ? '' : `=${type}`}]`,
-// 		delimiter = delimiterRegEx || /\{\{([\s\S]+?)\}\}/m;
-
-// 	[].forEach.call(element.querySelectorAll(interpolateType), (node) => {
-// 		if (!node.childNodes.length) return;
-		
-// 		[].filter.call(node.childNodes, (child) => {
-// 			return (child.nodeName === '#text' || child instanceof Text) && delimiter.test(child.textContent);
-// 		}).forEach((textNode) => { 
-// 			// we are containing all textnodes with interpolated values in a span,
-// 			// it takes considerable less effort to replace textnodes this way
-// 			let newNode = document.createElement('span');
-// 			newNode.insertAdjacentHTML('beforeend', interpolate(textNode.textContent, data));
-// 			textNode.parentNode.replaceChild(newNode, textNode);
-// 		});
-// 	});
-
-// 	return element;
-// }
-
 export const traverseTextNode: Function = (element: HTMLElement, query: string): HTMLElement[] => {
 	let nodes = [];
 
